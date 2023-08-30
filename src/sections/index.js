37860377e8addca2464f520250454a1f9af25252
Vinayak <vinayakcpa@gmail.com>
2023-08-30 18:36:23 -0400

    added component imports

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+
+export {
+  Hero,
+  PopularProducts,
+  Services,
+  Footer,
+  CustomerReviews,
+  SpecialOffer,
+  SuperQuality,
+  Subscribe,
+};
